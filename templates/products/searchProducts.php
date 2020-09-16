2a7fd3f7b51c34a181bc5db1656ef9ec285a4331
Werdlem <smrobins@virginmedia.com>
2020-09-16 16:03:08 +0100

    v20.10

--- templates/products/searchProducts.php
+++ templates/products/searchProducts.php
@@ -1,22 +1,27 @@
 
  <?php include '../menuItems/productsMenu.html'; ?>
- 
+ <style type="text/css">p {margin-bottom: 0.5rem;}</style>
 
 
-<div id="container" style="box-shadow: 4px 11px 13px 10px #d4d4d4; border-radius: 5px; padding: 15px; margin-top: 5px">
-<br>
+<div id="container" style="border: 1px solid rgba(0,0,255,0.2); background-color: rgba(0,0,255,0.1); border-radius: 5px; padding: 15px; margin-top: 5px">
+
 <form style="margin-top: 0em">
 	Search Product: <input type="text" class="form-control" style="width: 25%; display: inline;" ng-model="selectedProduct"> <button class="btn btn-primary mb-2" ng-click="searchProduct()">Search</button>
 </form>
-<div ng-show="pro.getProducts">
-<h1>Product Details</h1>
-
-	<p>Product Name: <a href="/productDetails?SkuID={{pro.getProducts[0].SkuID}}&Sku={{pro.getProducts[0].Sku}}">{{pro.getProducts[0].Sku}}</a></p>
-	<p>Sku Alias: <span ng-repeat="x in pro.getProducts"><strong>{{x.Alias}}</strong></span> <br/></p>
-	<p>Qty in Stock: {{pro.getProducts[0].StockQty}}</p>
-	<p>Location: <span ng-repeat="x in pro.getProducts"><strong> {{x.Location}} </strong><button type="button" ng-show="x.Location" class="btn btn-danger btn-sm" disabled>Delete </button></span></p>
-	<p>Product Description: {{pr.getProducts[0].description}}</p>
-	<p>Notes: {{pr.getProducts[0].Notes}}</p>
 </div>
+<div ng-show="pro.getProducts" style="width:80%">
+	<div id="container" style="box-shadow: 4px 11px 13px 10px #d4d4d4; border-radius: 5px; padding: 15px; margin-top: 20px">
+<h3 style="text-align: center;text-decoration: underline;">Search Results</h3 >
+ <div ng-repeat="x in pro.getProducts" style="border-bottom: 2px dashed #6c757d">
+
+
+	<p>Sku: <a href="/productDetails?SkuID={{x.SkuID}}&Sku={{x.Sku}}">{{x.Sku}}</a></p>
+	<p>Location: <strong>{{x.location}}</strong></p>
+	<p>Qty: {{x.StockQty}}</p>
+	<p>Alias: <strong>{{x.Alias}}</strong><br/></p>
+</div>
+
+</div>
+
 
 
